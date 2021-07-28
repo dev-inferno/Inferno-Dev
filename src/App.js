cd07a144e068c055bc7b90735cfe39a3a330fc54
= <thecakebaker52@gmail.com>
2021-07-28 09:20:52 -0400

    home page html

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Inferno Dev</h1>
+    <div className="center-container">
+      <h1 className="title">Inferno Dev</h1>
+      <div className="learn-button-container">
+        <button className="learn"><h3>Learn more about us</h3></button>
+      </div>
+    </div>
+    
   );
 }
 
