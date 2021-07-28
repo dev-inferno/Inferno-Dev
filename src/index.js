f9b46c851fe9a01a5103d15db1fc4b135efbb06a
= <thecakebaker52@gmail.com>
2021-07-28 09:27:17 -0400

    Switch to static html

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -12,3 +12,4 @@ ReactDOM.render(
 );
 
 
+*/
