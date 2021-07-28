b76ff093c417a38320e11a7a2fd8ff4e33db024e
= <thecakebaker52@gmail.com>
2021-07-28 09:56:30 -0400

    Added Header

--- src/App.js
+++ src/App.js
@@ -3,12 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="center-container">
-      <h1 className="title">Inferno Dev</h1>
-      <div className="learn-button-container">
-        <button className="learn"><h3>Learn more about us</h3></button>
-      </div>
-    </div>
+
+    <div></div>
     
   );
 }
